4caea3c9fab8d53031d56970ee94a8516566cf86
judge_happily <you3192001@gmail.com>
2024-03-07 16:17:06 +0900

    Initial Commit

---
+++ MapbeDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapbeDiary
+//
+//  Created by Jae hyung Kim on 3/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
