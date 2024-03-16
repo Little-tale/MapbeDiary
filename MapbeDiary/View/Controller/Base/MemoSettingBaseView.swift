d3b505e1a9d39df257de5f9e09359d5714f38365
judge_happily <you3192001@gmail.com>
2024-03-17 00:45:17 +0900

    Issue : 깃 재시도

--- MapbeDiary/View/Controller/Base/MemoSettingBaseView.swift
+++ MapbeDiary/View/Controller/Base/MemoSettingBaseView.swift
@@ -28,6 +28,25 @@ final class MemoSettingBaseView: BaseView {
         return view
     }()
     
+    let deleteButton: UIButton = {
+        var configuration = UIButton.Configuration.filled()
+        let view = UIButton(frame: .zero)
+        configuration.baseBackgroundColor = .red
+        configuration.title = "삭제"
+        view.configuration = configuration
+        return view
+    }()
+    
+    let backButton: UIButton = {
+        var configuration = UIButton.Configuration.plain()
+        let view = UIButton(frame: .zero)
+        
+        configuration.image = UIImage(systemName: "chevron.backward")
+        
+        view.configuration = configuration
+        return view
+    }()
+    
     let memoTextView = MemoTextview()
     
     let memoTextCountLabel = UILabel()
@@ -35,7 +54,7 @@ final class MemoSettingBaseView: BaseView {
     let imageCounterLabel = UILabel()
     
     let addImageButton: UIButton = {
-        var configuration = UIButton.Configuration.filled()
+        var configuration = UIButton.Configuration.tinted()
         let view = UIButton(frame: .zero)
         configuration.title = "이미지 추가"
         view.configuration = configuration
@@ -46,7 +65,9 @@ final class MemoSettingBaseView: BaseView {
     
     override func configureHierarchy() {
         addSubview(titleLabel)
+        addSubview(backButton)
         addSubview(saveButton)
+        addSubview(deleteButton)
         addSubview(memoTextView)
         addSubview(memoTextCountLabel)
         addSubview(imageCounterLabel)
@@ -59,11 +80,21 @@ final class MemoSettingBaseView: BaseView {
             make.centerX.equalTo(safeAreaLayoutGuide)
             make.top.equalTo(safeAreaLayoutGuide).offset(10)
         }
+        backButton.snp.makeConstraints { make in
+            make.leading.equalTo(safeAreaLayoutGuide).offset(10)
+            make.centerY.equalTo(titleLabel)
+        }
         saveButton.snp.makeConstraints { make in
             make.trailing.equalTo(memoTextView.snp.trailing)
-            make.top.equalTo(safeAreaLayoutGuide).offset(8)
-            make.height.equalTo(30)
+            make.centerY.equalTo(titleLabel)
+            make.height.equalTo(27)
         }
+        
+        deleteButton.snp.makeConstraints { make in
+            make.trailing.equalTo(saveButton.snp.leading).inset( -4 )
+            make.top.height.equalTo(saveButton)
+        }
+        
         memoTextView.snp.makeConstraints { make in
             make.top.equalTo(titleLabel.snp.bottom).offset(12)
             make.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(16)
@@ -113,6 +144,8 @@ final class MemoSettingBaseView: BaseView {
     
     override func register() {
         colletionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        
+        colletionView.register(OnlyImageCollectionViewCell.self, forCellWithReuseIdentifier: OnlyImageCollectionViewCell.reusebleIdentifier)
     }
 }
 
