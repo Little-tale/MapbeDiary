d6586e0647883ef9a938bb796dc5e904dd6e4831
judge_happily <you3192001@gmail.com>
2024-03-18 16:55:27 +0900

    UI View 개선

---
+++ MapbeDiary/Manager/CenterFlowLayout.swift
@@ -0,0 +1,55 @@
+//
+//  CenterFlowLayout.swift
+//  MapbeDiary
+//
+//  Created by Jae hyung Kim on 3/17/24.
+//
+
+import UIKit
+
+
+class CenterFlowLayout: UICollectionViewFlowLayout {
+    // MARK: 스크롤 동작이 완료 될때 contentOffSet을 결정
+    // proposedContentOffset -> 스크롤 동작이 끝날때 나온 값
+    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
+        // 컬렉션뷰가 없다면 기본동작으로
+        guard let collectionView = self.collectionView else {
+            return super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity)
+        }
+        // 컬렉션뷰 현재 바운드를 가져오고
+        let collectionViewBounds = collectionView.bounds
+        // 컬렉션뷰 위드를 절반으로하고
+        let harfW = collectionViewBounds.size.width * 0.5
+        // 오프셋 중앙위치를 계산
+        let contentOffsetX = proposedContentOffset.x + harfW
+        
+        // 현재 collectionView CGRect 영역 모든 레이아웃 속성 가져오기
+        let layoutAtr = layoutAttributesForElements(in: collectionViewBounds)
+        // 임시변수
+        var tempAtrr: UICollectionViewLayoutAttributes?
+        if let layoutAtr{
+            // 모든 영역 즉 헤더, 푸터 등이 있어서 그중 셀만 처리할수 있게 한다.
+            for attri in layoutAtr {
+                if attri.representedElementCategory != .cell {
+                    continue
+                }
+                
+                // 첫번째 셀인경우 후보로 등록
+                if tempAtrr == nil {
+                    tempAtrr = attri
+                    continue
+                }
+                
+                let first = attri.center.x - contentOffsetX
+                let second = tempAtrr!.center.x - contentOffsetX
+                // 절대값으로 변환 후 비교
+                if abs(first) < abs(second){
+                    tempAtrr = attri
+                }
+            }  // 이부분도 꼭 회고!
+            return CGPoint(x: (tempAtrr?.center.x ?? 0) - harfW, y: proposedContentOffset.y)
+        } else {
+            return super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity)
+        }
+    }
+}
---
+++ MapbeDiary/View/Controller/Base/Sub/CustomImageViewer.swift
@@ -0,0 +1,94 @@
+//
+//  CustomImageViewer.swift
+//  MapbeDiary
+//
+//  Created by Jae hyung Kim on 3/15/24.
+//
+
+import UIKit
+import SnapKit
+
+final class CustomImageViewer: UIViewController, UIScrollViewDelegate {
+    let scrollView = UIScrollView()
+    let imageView = UIImageView()
+    let backButton = UIButton()
+    // MARK: 회고 -> 내일 회고로
+    let activityIndicator = UIActivityIndicatorView(style: .large)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        view.addSubview(backButton)
+        view.backgroundColor = .black
+        configureLayout()
+        
+        activityIndicator.center = view.center
+        activityIndicator.startAnimating()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+       
+        settingScollerView()
+        backButtonSetting()
+        imageView.frame = scrollView.bounds
+    }
+    
+    private func settingScollerView(){
+        scrollView.delegate = self
+        // 최대 최소 확대 비율 설정
+        scrollView.maximumZoomScale = 3.0
+        scrollView.minimumZoomScale = 0.8
+        
+        // 확대 - 축소 바운드 효과 활성화
+        scrollView.bouncesZoom = true
+        // 수평 - 수직 스크롤 인디케이터를 숨기기
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.backgroundColor = .black
+        imageView.contentMode = .scaleAspectFit
+    }
+    
+    private func backButtonSetting() {
+        var config = UIButton.Configuration.plain()
+        config.image = UIImage(systemName:"chevron.backward")
+        config.baseForegroundColor = .white
+        backButton.configuration = config
+        backButton.addAction(UIAction(handler: { [weak self] _ in
+            guard let self else { return }
+            dismiss(animated: true)
+        }), for: .touchUpInside)
+    }
+    
+    func configureLayout(){
+        backButton.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(10)
+            make.leading.equalTo(view.safeAreaLayoutGuide).offset(10)
+            make.size.equalTo(40)
+        }
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    
+    }
+    
+    // 스크롤 뷰에서 확대/ 축소 뷰를 지정
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func loadImage(data: Data) {
+        DispatchQueue.global().async { [weak self ] in
+            guard let self else { return }
+            DispatchQueue.main.async {
+                self.imageView.image = UIImage(data: data)
+                self.activityIndicator.stopAnimating()
+                print("불러옴")
+                self.activityIndicator.isHidden = true
+            }
+        }
+    }
+    
+}
